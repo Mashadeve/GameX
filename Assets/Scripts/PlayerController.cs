f0ce856bb375b3ac5e66c36d066235949d89b66a
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2021-03-05 14:49:23 +0200

    miekka ja movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,19 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveHorizontal, moveVertical;
+    public float moveHorizontal, moveVertical, jump;
+    [SerializeField]
     public float movementSpeed = 2.0f;
+    [SerializeField]
+    public float jumpForce = 10f;
+
     private Rigidbody2D rb;
+
+    private bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }       
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {       
+        {
+            isGrounded = false;
+        }      
+    }
     // Update is called once per frame
     void Update()
     {
         moveHorizontal = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(moveHorizontal * movementSpeed, rb.velocity.y);        
+        rb.velocity = new Vector2(moveHorizontal * movementSpeed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }       
     }
 }
