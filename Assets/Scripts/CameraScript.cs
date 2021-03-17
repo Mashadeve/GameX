fda57ae003eeea0e2c525e4879df25dabba5e426
Samu <65758544+SmuuSka@users.noreply.github.com>
2021-03-17 12:31:29 +0200

    Kamera

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private Camera playerCamera;
+    [SerializeField] private GameObject player;
+
+    private void Start()
+    {
+        playerCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+    private void Update()
+    {
+        playerCamera.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10f);
+        Debug.Log("Kamera sijaitsee " + playerCamera.transform.position);
+    }
+}
