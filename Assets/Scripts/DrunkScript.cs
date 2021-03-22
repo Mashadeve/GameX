dbabaa0528b0f02dfff01917d77d1cbdde3cc86d
Samu <65758544+SmuuSka@users.noreply.github.com>
2021-03-22 10:29:39 +0200

    moi

--- Assets/Scripts/DrunkScript.cs
+++ Assets/Scripts/DrunkScript.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DrunkScript : MonoBehaviour
 {
+    [SerializeField] Image pointer;
+    private int end1, end2;
     // Start is called before the first frame update
     void Start()
     {
+        end1 = -247;
+        end2 = 247;
         
     }
 
