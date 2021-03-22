9bf6aa53033ae76ab44a91fcf47690b3907aa1f6
Samu <65758544+SmuuSka@users.noreply.github.com>
2021-03-22 11:17:32 +0200

    moi

--- Assets/Scripts/DrunkScript.cs
+++ Assets/Scripts/DrunkScript.cs
@@ -6,12 +6,14 @@ using UnityEngine.UI;
 public class DrunkScript : MonoBehaviour
 {
     [SerializeField] Image pointer;
-    private int end1, end2;
+    private int end1, end2, currentPoints;
     // Start is called before the first frame update
     void Start()
     {
-        end1 = -247;
-        end2 = 247;
+        end1 = -240;
+        end2 = 240;
+
+        StartCoroutine(Timer());
         
     }
 
@@ -19,5 +21,29 @@ public class DrunkScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private IEnumerator Timer()
+    {
+        currentPoints = 0;
+        while (true)
+        {
+            Debug.Log(currentPoints);
+            yield return new WaitForSeconds(2f);
+            currentPoints -= 5;
+            pointer.GetComponent<RectTransform>().transform.position = new Vector2(pointer.GetComponent<RectTransform>().transform.position.x - pointer.gameObject.GetComponent<RectTransform>().rect.width, pointer.GetComponent<RectTransform>().transform.position.y);
+
+
+            if (currentPoints < end1)
+            {
+                Debug.Log("You arent drunk at all");
+                break;
+            }
+            if (currentPoints > end2)
+            {
+                Debug.Log("You are way too drunk");
+                break;
+            }
+        }
     }
 }
