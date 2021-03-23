cbd1c8b71dc6cc2c89cfe6959964860fd3b097d6
Samu <65758544+SmuuSka@users.noreply.github.com>
2021-03-23 14:31:00 +0200

    Revert "Worldstart ja end"
    This reverts commit f8504d0ed592d47e9d19a7fa6d97a425722775ec.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,47 +4,22 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private Camera playerCamera;
+    private Camera playerCamera;
     [SerializeField] private PlayerController player;
     [SerializeField] private float offSet;
-    [SerializeField] private Transform worldStart, worldEnd;
-    private Vector3 minX, maxX;
-    
-       
+
     void Start()
     {
-
-        playerCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
-        worldStart = GetComponent<Transform>();
-        worldEnd = GetComponent<Transform>();
-        //float width = Camera.main.orthographicSize * Screen.width / Screen.height;
-        playerCamera.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
-
+        playerCamera = GameObject.Find("Main Camera").GetComponent<Camera>();        
     }
 
     void Update()
     {
         if (player.playerAlive)
         {
-            playerCamera.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + offSet, -10f);
+            playerCamera.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + offSet, -15f);
         }
-
-        WorldPos();
+        
         
     }
-
-    private void WorldPos()
-    {
-        
-        if (player.transform.position.x < worldStart.transform.position.x)
-        {
-            playerCamera.transform.position = new Vector3(worldStart.transform.position.x, playerCamera.transform.position.y, -10f);
-        }
-
-        if (-player.transform.position.x > -worldEnd.transform.position.x)
-        {
-            playerCamera.transform.position = new Vector3(worldEnd.transform.position.x, playerCamera.transform.position.y, -10f);
-        }
-    }
-
 }
