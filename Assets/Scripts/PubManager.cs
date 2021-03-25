2944ee47698af26a2f48c356719e7330683ae0e8
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2021-03-25 15:14:43 +0200

    ovisound

---
+++ Assets/Scripts/PubManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PubManager : MonoBehaviour
+{
+    [SerializeField] PlayerController player;
+    private bool ovi;
+
+    void Start()
+    {
+        ovi = true;
+    }
+
+    
+    void Update()
+    {
+               
+         if (player.canMove == true && ovi == true)
+         {
+            GameObject.Find("Baari_ovi_kiinni").GetComponent<AudioSource>().Play();
+            GameObject.Find("Baari_ovi_auki").SetActive(false);           
+            ovi = false;
+         }                    
+    }
+}
