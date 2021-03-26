2c10097e3b7588fb6948f9c9ff16794e55ed558b
Samu <65758544+SmuuSka@users.noreply.github.com>
2021-03-26 13:52:51 +0200

    Revert "Merge branch 'Paivitykset' into Samu"
    This reverts commit dde90541d6c670f50eeabdc329f6da413b3f3a99, reversing
    changes made to a47f1d27003c5992833dd200043286192b35c550.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,14 +7,32 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private GameObject playerHealth;
-    [SerializeField] private GameObject pausePanel;   
+    [SerializeField] private GameObject pausePanel;
+    public static UIManager instance;
 
     private void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+        }
 
     }
 
 
+    public void OnPause()
+    {
+        pausePanel.SetActive(!pausePanel.activeSelf);
+
+        if (pausePanel.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
 
     public void PlayGame()
     {
