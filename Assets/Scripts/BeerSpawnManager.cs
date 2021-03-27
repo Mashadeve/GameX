8150be556e57e4442468a265a956e506e39bc13a
Samu <65758544+SmuuSka@users.noreply.github.com>
2021-03-27 11:25:45 +0200

    moi

--- Assets/Scripts/BeerSpawnManager.cs
+++ Assets/Scripts/BeerSpawnManager.cs
@@ -6,7 +6,7 @@ public class BeerSpawnManager : MonoBehaviour
 {
     public GameObject myPrefab;
     public int beerCount;
-    private Vector3 Pos1, Pos2, Pos3;
+    private Vector3 Pos1, Pos2, Pos3, Pos4, Pos5;
     public bool canSpawn = true;
 
     private void Start()
@@ -15,6 +15,8 @@ public class BeerSpawnManager : MonoBehaviour
         Pos1 = new Vector3(17, 0.5f, 0);
         Pos2 = new Vector3(65, -3, 0);
         Pos3 = new Vector3(100, -3, 0);
+        Pos4 = new Vector3(200, 2, 0);
+        Pos5 = new Vector3(300, 1, 0);
 
     }
 
@@ -35,6 +37,16 @@ public class BeerSpawnManager : MonoBehaviour
         if (beerCount == 2 && canSpawn == true)
         {
             CloneBeer(Pos3);
+            canSpawn = false;
+        }
+        if (beerCount == 3 && canSpawn == true)
+        {
+            CloneBeer(Pos4);
+            canSpawn = false;
+        }
+        if (beerCount == 4 && canSpawn == true)
+        {
+            CloneBeer(Pos5);
             canSpawn = false;
         }
     }
