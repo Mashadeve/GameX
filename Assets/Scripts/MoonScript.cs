9dc5d953c6d0e81e093d6cd3f34077eafde72434
Samu <65758544+SmuuSka@users.noreply.github.com>
2021-03-27 09:43:28 +0200

    moi

---
+++ Assets/Scripts/MoonScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonScript : MonoBehaviour
+{
+    [SerializeField] private Transform followTarget;
+    private float smoothTime = 0.5f;
+    private Vector3 velocity = Vector3.zero;
+
+    private void Update()
+    {
+        Vector3 targetPosition = followTarget.TransformPoint(new Vector3(followTarget.position.x, 0, 0));
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
