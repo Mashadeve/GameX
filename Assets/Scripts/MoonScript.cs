abe8074ad65fadc2afae16748345950b850dadb0
Samu <65758544+SmuuSka@users.noreply.github.com>
2021-03-27 10:55:19 +0200

    moi

--- Assets/Scripts/MoonScript.cs
+++ Assets/Scripts/MoonScript.cs
@@ -15,8 +15,8 @@ public class MoonScript : MonoBehaviour
 
     private void Update()
     {
-        Vector3 targetPosition = followTarget.TransformPoint(0, 20, 0);
+        Vector3 targetPosition = followTarget.TransformPoint(0, 0, 0);
 
-        transform.position = Vector3.SmoothDamp(new Vector3(gameObject.transform.position.x, 2, 0), targetPosition, ref velocity, smoothTime);
+        transform.position = Vector3.SmoothDamp(new Vector3(followTarget.position.x + 4f , 2 ,0), targetPosition, ref velocity, smoothTime, Time.deltaTime);
     }
 }
