bc3164cad55d96e760dfc976f8abe45637661d1c
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2021-03-27 09:43:30 +0200

    WinScreen

---
+++ Assets/Scripts/WinScreenManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreenManager : MonoBehaviour
+{
+    [SerializeField] Collider2D IsFinished;
+    
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {            
+            SceneManager.LoadScene("WinScreen");
+            Debug.Log(collision + "osuu");
+        }
+    }
+}
